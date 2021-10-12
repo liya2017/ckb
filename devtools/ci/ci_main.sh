f8609eb43288f33ddf10d9786600dbbc4392dea6
liya2017 <Liya@cryptape.com>
2021-10-12 16:44:16 +0800

    feat: Automatically create issue when audit check failed

--- devtools/ci/ci_main.sh
+++ devtools/ci/ci_main.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -u
+set -euo pipefail
 is_self_runner=`echo $RUNNER_LABEL | awk -F '-' '{print $1}'`
 if [[ $is_self_runner == "self" ]];then
   CARGO_TARGET_DIR=$GITHUB_WORKSPACE/../target
@@ -97,111 +97,31 @@ ci_integration_tests*)
     cd test
     if [[ $github_workflow_os == 'windows' ]];then
       cargo run -- --bin ${CARGO_TARGET_DIR}/release/ckb.exe --log-file ${GITHUB_WORKSPACE}/integration.log ${CKB_TEST_ARGS}
-      if [ $? -eq 0 ]; then
-	       printf "SUCCESS\n"
-      else
-	       printf "FAILURE\n"
-	       EXIT_CODE=1
-      fi
     else
       cargo run -- --bin ${CARGO_TARGET_DIR}/release/ckb --log-file ${GITHUB_WORKSPACE}/integration.log ${CKB_TEST_ARGS}
-      if [ $? -eq 0 ]; then
-	       printf "SUCCESS\n"
-      else
-	       printf "FAILURE\n"
-	       EXIT_CODE=1
-      fi
     fi
     ;;
   ci_quick_checks*)
     echo "ci_quick_check"
     make check-cargotoml
-    if [ $? -eq 0 ]; then
-	     printf "SUCCESS\n"
-    else
-	     printf "FAILURE\n"
-	     EXIT_CODE=1
-    fi
     make check-whitespaces
-    if [ $? -eq 0 ]; then
-	     printf "SUCCESS\n"
-    else
-	     printf "FAILURE\n"
-	     EXIT_CODE=1
-    fi
     make check-dirty-rpc-doc
-    if [ $? -eq 0 ]; then
-	     printf "SUCCESS\n"
-    else
-	     printf "FAILURE\n"
-	     EXIT_CODE=1
-    fi
     make check-dirty-hashes-toml
-    if [ $? -eq 0 ]; then
-	     printf "SUCCESS\n"
-    else
-	     printf "FAILURE\n"
-	     EXIT_CODE=1
-    fi
     devtools/ci/check-cyclic-dependencies.py
-    if [ $? -eq 0 ]; then
-	     printf "SUCCESS\n"
-    else
-	     printf "FAILURE\n"
-	     EXIT_CODE=1
-    fi
     ;;
   ci_wasm_build*)
     echo "ci_WASM_build"
-    rustup target add wasm32-unknown-unknown
-    if [ $? -eq 0 ]; then
-	     printf "SUCCESS\n"
-    else
-	     printf "FAILURE\n"
-	     EXIT_CODE=1
-    fi
+    rustup target add wasm32-unknown-unknownXIT_CODE=1
     make wasm-build-test
-    if [ $? -eq 0 ]; then
-	     printf "SUCCESS\n"
-    else
-	     printf "FAILURE\n"
-	     EXIT_CODE=1
-    fi
     ;;
   ci_cargo_deny*)
     echo "ci_security_audit_licenses"
     cargo deny --version || cargo install cargo-deny --locked
-    if [ $? -eq 0 ]; then
-	     printf "SUCCESS\n"
-    else
-	     printf "FAILURE\n"
-	     EXIT_CODE=1
-    fi
     make security-audit
-    if [ $? -eq 0 ]; then
-	     printf "SUCCESS\n"
-    else
-	     printf "FAILURE\n"
-	     EXIT_CODE=1
-    fi
     make check-crates
-    if [ $? -eq 0 ]; then
-	     printf "SUCCESS\n"
-    else
-	     printf "FAILURE\n"
-	     EXIT_CODE=1
-    fi
     make check-licenses
-    if [ $? -eq 0 ]; then
-	     printf "SUCCESS\n"
-    else
-	     printf "FAILURE\n"
-	     EXIT_CODE=1
-    fi
     ;;
   *)
     echo -n "unknown"
     ;;
 esac
-echo " EXIT_CODE is "$EXIT_CODE
-exit $EXIT_CODE
