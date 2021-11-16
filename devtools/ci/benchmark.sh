2b9254328361a61aab4206828976e83871dfaa43
liya2017 <Liya@cryptape.com>
2021-11-16 13:49:08 +0800

    ci: benchmark update

---
+++ devtools/ci/benchmark.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -euo pipefail
+SCRIPT_PATH="$GITHUB_WORKSPACE/ckb-integration-test/ckb-bench"
+JOB_ID="benchmark-$(date +'%Y-%m-%d')-in-10h"
+JOB_DIRECTORY="$SCRIPT_PATH/job/$JOB_ID"
+ANSIBLE_DIR="$GITHUB_WORKSPACE/ansible"
+mkdir $ANSIBLE_DIR
+echo "ANSIBLE_DIR=$ANSIBLE_DIR" >> $GITHUB_ENV
+function benchmark() {
+    $SCRIPT_PATH/script/benchmark.sh run
+    cp $JOB_DIRECTORY/ansible/ckb-bench.log $ANSIBLE_DIR
+    # TODO: copy report.yml to $ANSIBLE_DIR
+    $SCRIPT_PATH/script/benchmark.sh clean
+}
+
+function github_report_error() {
+    $SCRIPT_PATH/script/ok.sh add_comment nervosnetwork/ckb 2372 "**Sync-Mainnet Report**:\nSync-Mainnet crashed"
+
+    # double check
+    $SCRIPT_PATH/script/benchmark.sh clean
+}
+
+function main() {
+    benchmark || github_report_error
+}
+main $*
